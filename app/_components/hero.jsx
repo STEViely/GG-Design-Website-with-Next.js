1451f3551473c91770e84101d74f5d4f61feb32c
Ben S.Teerawar <stevielyfiles@gmail.com>
2024-09-12 08:01:12 +0700

    update hero

--- app/_components/hero.jsx
+++ app/_components/hero.jsx
@@ -30,7 +30,7 @@ export default function HeroSection({ targetSectionRef1, targetSectionRef2 }) {
         <div className="flex gap-4">
           <div
             onClick={scrollToSection1}
-            className="border border-white  rounded-lg p-4 w-[50%] hover:bg-white hover:text-[rgba(0,19,59,0.8)] flex flex-col  items-center justify-center md:w-[300px] md:h-[250px] lg:w-[350px] lg:h-[300px] xl:w-[500px] xl: 2xl:w-[600px] 2xl:h-[400px]"
+            className="border border-white  rounded-lg p-4 w-[50%] hover:bg-white cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105 hover:text-[rgba(0,19,59,0.8)] flex flex-col  items-center justify-center md:w-[300px] md:h-[250px] lg:w-[350px] lg:h-[300px] xl:w-[500px] xl: 2xl:w-[600px] 2xl:h-[400px]"
           >
             <h1 className="text-[14px] md:text-[20px] lg:text-[28px] xl:text-[40px] 2xl:text-[45px]">
               Logo Design
@@ -41,7 +41,7 @@ export default function HeroSection({ targetSectionRef1, targetSectionRef2 }) {
           </div>
           <div
             onClick={scrollToSection2}
-            className="border border-white p-4 w-[50%] rounded-lg hover:bg-white hover:text-[rgba(0,19,59,0.8)] flex flex-col  items-center justify-center md:w-[300px] md:h-[250px] lg:w-[350px] lg:h-[300px] xl:w-[500px] xl: 2xl:w-[600px] 2xl:h-[400px]"
+            className="border border-white p-4 w-[50%] rounded-lg hover:bg-white cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105 hover:text-[rgba(0,19,59,0.8)] flex flex-col  items-center justify-center md:w-[300px] md:h-[250px] lg:w-[350px] lg:h-[300px] xl:w-[500px] xl: 2xl:w-[600px] 2xl:h-[400px]"
           >
             <h1 className="text-[14px] md:text-[20px] lg:text-[28px] xl:text-[40px] 2xl:text-[45px]">
               Logo Design
