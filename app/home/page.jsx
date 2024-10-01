7e5e285a4574662d2bd82961e1387ac7ea78f714
Ben S.Teerawar <stevielyfiles@gmail.com>
2024-10-01 23:29:09 +0700

    added carousel

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,3 +1,12 @@
+"use client";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EmblaCarousel from "../_components/EmblaCarousel";
+
+import "../../css/base.css";
+import "../../css/sandbox.css";
+import "../../css/embla.css";
 import Image from "next/image";
 import Link from "next/link";
 import Bgg from "@/public/LandingPage/bgg.jpg";
@@ -17,14 +26,22 @@ import G2 from "@/public/LandingPage/GG 2-2.jpg";
 import G3 from "@/public/LandingPage/GG 2-3.jpg";
 import G4 from "@/public/LandingPage/GG 2-4.jpg";
 import G5 from "@/public/LandingPage/GG 2-5.jpg";
+import Typewriter from "typewriter-effect";
+
+const OPTIONS = { loop: true };
 
 export default function Page() {
   const images = [
     { src: A1, alt: "Main Picture" },
+    { src: G1, alt: "Main Picture" },
     { src: A2, alt: "Main Picture" },
+    { src: G2, alt: "Main Picture" },
     { src: A3, alt: "Main Picture" },
+    { src: G3, alt: "Main Picture" },
     { src: A4, alt: "Main Picture" },
+    { src: G4, alt: "Main Picture" },
     { src: A5, alt: "Main Picture" },
+    { src: G5, alt: "Main Picture" },
     { src: A6, alt: "Main Picture" },
     { src: A7, alt: "Main Picture" },
     { src: A8, alt: "Main Picture" },
@@ -32,84 +49,60 @@ export default function Page() {
     { src: A10, alt: "Main Picture" },
   ];
 
-  const webs = [
-    { src: G1, alt: "Web picture" },
-    { src: G2, alt: "Web picture" },
-    { src: G3, alt: "Web picture" },
-    { src: G4, alt: "Web picture" },
-    { src: G5, alt: "Web picture" },
-  ];
-
   return (
-    <div className="relative w-full h-screen">
-      {/* Background Image */}
-      <Image
-        src={Bgg}
-        alt="background"
-        fill
-        style={{ objectFit: "cover" }}
-        priority
-        className="opacity-30"
-      />
+    <main className="flex flex-col items-center justify-between">
+      <div className="relative w-full h-screen flex flex-col items-center">
+        <div className="absolute -z-10 w-full h-full">
+          <Image
+            src={Bgg}
+            alt="background"
+            layout="fill"
+            objectFit="cover"
+            className="opacity-30"
+          />
+        </div>
+        <div>
+          <Image
+            src={GGD2}
+            alt="Logo"
+            width={129}
+            height={129}
+            className="mx-auto md:w-[240px] lg:w-[320px] 2xl:w-[300px]"
+          />
+        </div>
+        <div className="w-[80%]">
+          <div className="flex justify-center">
+            <h1 className="text-[28px] md:text-[40px] lg:text-[56px] 2xl:text-[58px]">
+              <Typewriter
+                options={{
+                  strings: [
+                    "ออกเเบบโลโก้",
+                    "ออกแบบเว็บไซต์",
+                    "สร้างเว็บไซต์เเละระบบ",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
+          </div>
+          <div className="my-4 mx-auto">
+            <p className="md:text-[24px] lg:text-[32px] 2xl:text-[34px]">
+              “ GG Design ” บริการออกแบบโลโก้, ออกแบบและสร้างเว็บไซต์
+              เพื่อให้ธุรกิจกของคุณ มีความน่าเชื่อถือและเป็นมืออาชีพ
+              ในราคาที่เข้าถึงง่าย ยินดีให้คำปรึกษาฟรี
+            </p>
+          </div>
 
-      {/* Centered Logo */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4">
-        <Image src={GGD2} alt="logo" width={303} height={303} />
-      </div>
+          <EmblaCarousel slides={images} options={OPTIONS} />
 
-      {/* Responsive text */}
-      <div className="absolute top-[180px] md:top-[280px] left-1/2 transform -translate-x-1/2 text-center">
-        <h1 className="text-2xl md:text-[48px]">บริการออกเเบบ</h1>
-      </div>
-
-      {/* Responsive description text */}
-      <div className="w-full px-4 md:w-[710px] md:h-[114px]">
-        <p className="text-[14px] md:text-[20px] lg:text-[20px] 2xl:text-[28px] absolute top-[200px] md:top-[320px] lg:top-[320px] left-1/2 transform -translate-x-1/2 mt-4 text-center">
-          <b>“ GG Design ”</b> บริการออกแบบโลโก้, ออกแบบและสร้างเว็บไซต์{" "}
-          <br className="lg:hidden md:hidden" />
-          เพื่อให้ธุรกิจกของคุณ มีความน่าเชื่อถือและเป็นมืออาชีพ
-          ในราคาที่เข้าถึงง่าย ยินดีให้คำปรึกษาฟรี
-        </p>
-      </div>
-
-      {/* Centered Images - Responsive */}
-      <div className="absolute w-full flex justify-center top-[320px] md:top-[450px] lg:top-[420px]  2xl:top-[420px]">
-        <div className="grid grid-cols-5 md:grid-cols-5 gap-[5px] justify-center mt-4">
-          {images.map((el, index) => (
-            <Image
-              src={el.src}
-              alt={el.alt}
-              width={148.83}
-              height={148.83}
-              key={index}
-              className="drop-shadow-lg w-[50px] md:w-[120px] 2xl:w-[100px]"
-            />
-          ))}
+          <Link href="/" className="flex justify-center mt-10">
+            <button className="bg-[#00133B] w-[120px] h-[32px] text-white rounded-[42px] hover:transition-transform duration-300 ease-in-out hover:scale-105 md:w-[170px] md:h-[52px] md:text-[24px] lg:w-[250px] lg:h-[70px] lg:text-[32px] xl:w-[250px] 2xl:w-[250px] 2xl:h-[60px] 2xl:text-[32px]">
+              เข้าสู่เว็บไซต์
+            </button>
+          </Link>
         </div>
       </div>
-
-      {/* Web Images - Responsive */}
-      <div className="absolute w-full flex justify-center top-[440px] md:top-[720px] lg:top-[600px] 2xl:top-[640px]">
-        <div className="grid grid-cols-5 md:grid-cols-5 gap-[5px] justify-center mt-4">
-          {webs.map((el, index) => (
-            <Image
-              src={el.src}
-              alt={el.alt}
-              width={251.33}
-              height={177.82}
-              key={index}
-              className="drop-shadow-lg w-[60px] md:w-[140px] 2xl:w-[180px]"
-            />
-          ))}
-        </div>
-      </div>
-
-      {/* Button */}
-      <Link href="/">
-        <button className="absolute bottom-[80px] md:bottom-[80px] lg:bottom-[40px] left-1/2 transform -translate-x-1/2 w-[150px] md:w-[172px] h-[40px] md:h-[43px] rounded-[42px] bg-[#00133B] text-white text-[16px] md:text-[24px] drop-shadow-lg hover:transition-transform duration-300 ease-in-out hover:scale-105">
-          เข้าสู่เว็บไซต์
-        </button>
-      </Link>
-    </div>
+    </main>
   );
 }
